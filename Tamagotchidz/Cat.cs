0f3f4a78a55b7d84b0cad09d918ed058d0d26dc0
GolikovaVV <GolikovaVV@302D-14>
2023-03-02 16:16:47 +0700

    Добавьте файлы проекта.

---
+++ Tamagotchidz/Cat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tamagotchidz
+{
+    struct Fruit
+    {
+        public string type;
+        public int hunger;
+
+    }
+    
+    class Cat
+    {
+        public void Meow() //кошка может мяукать
+        {
+            Console.WriteLine("meow! ");
+        }
+        public void Eat(Fruit f)
+        {
+            Console.WriteLine($"Cat eats{f.type}. {f.health} hp.");
+        }
+        public void Play()
+        {
+            hunger -= 30;
+            Console.WriteLine($"stop feeding the cat" + hunger);
+
+        }
+        public void Run()
+        {
+            hunger -= 0;
+            Console.WriteLine($"need to feed the cat" + hunger");
+        }
+    } 
+}       
+         
+      
---
+++ Tamagotchidz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tamagotchidz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// создали кошку
+            Cat cat = new Cat();
+            cat.Meow(); //помяукала
+            // создали фрукт яблоко
+            Fruit fruitApple = new Fruit { health = 10 };
+            // создали фрукт апельсин
+            cat.Eat(fruitApple);
+            Fruit fruitOrange = new Fruit { health = 5 };
+            cat.Eat(fruitOrange);
+            DoMeal(cat, fruitApple); // покормили яблоком
+            DoMeal(cat, fruitApple); // покормили яблоком
+            DoMeal(cat, fruitOrange); // покормили апельсином
+            cat.Play();
+            cat.Run();
+
+        }
+        static void DoMeal(Cat c, Fruit f)
+        {
+            c.Eat(f); //кошка {имя кошки} съела фрукт
+        }
+
+    }
+}
